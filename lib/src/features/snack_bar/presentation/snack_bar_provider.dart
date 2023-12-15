5d7ad6c3815079c15d6ba62359ea6eaca8a0f5d3
Guillaume LE MARTRET <guillaume@hoalen.com>
2023-12-15 20:19:46 +0100

    feat: add snakBar provider

---
+++ lib/src/features/snack_bar/presentation/snack_bar_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'snack_bar_provider.g.dart';
+
+@riverpod
+SnackBar? snackBar(SnackBarRef bar) => null;
--- lib/src/features/snack_bar/presentation/snack_bar_provider.g.dart
+++ lib/src/features/snack_bar/presentation/snack_bar_provider.g.dart
@@ -1,24 +1,24 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'dio_provider.dart';
+part of 'snack_bar_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$dioHash() => r'907a953b4b1d1d8e0ffe7054500f6563afe3a4a8';
+String _$snackBarHash() => r'a331b9e8fd4283f73503e8c5c854b87f44870cc8';
 
-/// See also [dio].
-@ProviderFor(dio)
-final dioProvider = AutoDisposeProvider<Dio>.internal(
-  dio,
-  name: r'dioProvider',
+/// See also [snackBar].
+@ProviderFor(snackBar)
+final snackBarProvider = AutoDisposeProvider<SnackBar?>.internal(
+  snackBar,
+  name: r'snackBarProvider',
   debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$dioHash,
+      const bool.fromEnvironment('dart.vm.product') ? null : _$snackBarHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef DioRef = AutoDisposeProviderRef<Dio>;
+typedef SnackBarRef = AutoDisposeProviderRef<SnackBar?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
